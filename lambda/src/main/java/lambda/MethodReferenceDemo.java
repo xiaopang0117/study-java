74e1543174549cf940e9f7bc2bc38cd872d32152
jiangkuan <jiangkuan@chehejia.com>
2019-12-01 00:23:47 +0800

    feat:jdk会将当前实例传入每个非静态方法,作为方法的第一个参数,方法名为this

--- lambda/src/main/java/lambda/MethodReferenceDemo.java
+++ lambda/src/main/java/lambda/MethodReferenceDemo.java
@@ -1,9 +1,6 @@
 package lambda;
 
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.IntUnaryOperator;
-import java.util.function.UnaryOperator;
+import java.util.function.*;
 
 /**
  * 方法引用
@@ -26,10 +23,11 @@ class Dog {
 
     /**
      * 吃狗粮
+     * JDK默认会把当前实例传入非静态方法，参数名为this,第一个参数
      * @param num
      * @return 还剩多少斤
      */
-    public int eat(int num){
+    public int eat(Dog this,int num){
         System.out.println("吃了"+num+"斤狗粮");
         this.food -= num;
         return this.food;
@@ -45,13 +43,13 @@ class Dog {
 public class MethodReferenceDemo {
 
     public static void main(String[] args) {
+        Dog dog = new Dog();
         //方法引用
         Consumer<String> consumer = System.out::println;
         consumer.accept("789");
 
         //静态方法的方法引用（类名+方法名）
         Consumer<Dog> consumer1 = Dog::bark;
-        Dog dog = new Dog();
         consumer1.accept(dog);
 
         //非静态方法(对象实例的方法引用)
@@ -61,5 +59,8 @@ public class MethodReferenceDemo {
         //System.out.println("还剩下"+function.apply(5)+"斤");
         IntUnaryOperator function = dog::eat;
         System.out.println("还剩下"+function.applyAsInt(2)+"斤");
+        //使用类名来方法引用
+        BiFunction<Dog,Integer,Integer> function1 = Dog::eat;
+        System.out.println("还剩下"+function1.apply(dog,2)+"斤");
     }
 }
